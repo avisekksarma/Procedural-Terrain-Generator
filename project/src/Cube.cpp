8f64af86b5ae832faff21004770d0b6ecadc2c08
adh-aashish <adhikariaashish12@gmail.com>
2022-07-27 22:53:11 +0545

    glMath errors fixed

--- project/src/Cube.cpp
+++ project/src/Cube.cpp
@@ -1,36 +1,42 @@
 #include "../include/cube.h"
+#include "../include/glMath.h"
 
-Cube::Cube(sf::RenderWindow& w): window(&w) {
+Cube::Cube(sf::RenderWindow& w): window(&w),model(glMath::mat4f(1.0f)) {
+
+	// meshCube.tris = {
+	// 	// SOUTH
+	// 	{ glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f) },
+	// 	{ glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 0.0f, 0.0f) },
+
+	// 	// EAST                                                      
+	// 	{ glMath::vec3f(1.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f) },
+	// 	{ glMath::vec3f(1.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f),    glMath::vec3f(1.0f, 0.0f, 1.0f) },
+
+	// 	// NORTH                                                     
+	// 	{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f) },
+	// 	{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 1.0f) },
+
+	// 	// WEST                                                      
+	// 	{ glMath::vec3f(0.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 0.0f) },
+	// 	{ glMath::vec3f(0.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(0.0f, 0.0f, 0.0f) },
+
+	// 	// TOP                                                       
+	// 	{ glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f) },
+	// 	{ glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f) },
+
+	// 	// BOTTOM                                                    
+	// 	{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 0.0f) },
+	// 	{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 0.0f, 0.0f) },
+
+	// };
 
 	meshCube.tris = {
-		// SOUTH
-		{ glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f) },
-		{ glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 0.0f, 0.0f) },
-
-		// EAST                                                      
-		{ glMath::vec3f(1.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f) },
-		{ glMath::vec3f(1.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f),    glMath::vec3f(1.0f, 0.0f, 1.0f) },
-
-		// NORTH                                                     
-		{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f) },
-		{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 1.0f) },
-
-		// WEST                                                      
-		{ glMath::vec3f(0.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 0.0f) },
-		{ glMath::vec3f(0.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(0.0f, 0.0f, 0.0f) },
-
-		// TOP                                                       
-		{ glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(0.0f, 1.0f, 1.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f) },
-		{ glMath::vec3f(0.0f, 1.0f, 0.0f),    glMath::vec3f(1.0f, 1.0f, 1.0f),    glMath::vec3f(1.0f, 1.0f, 0.0f) },
-
-		// BOTTOM                                                    
-		{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 0.0f) },
-		{ glMath::vec3f(1.0f, 0.0f, 1.0f),    glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(1.0f, 0.0f, 0.0f) },
-
+		{ glMath::vec3f(0.0f, 0.0f, 0.0f),    glMath::vec3f(0.0f, 100.0f, 0.0f),    glMath::vec3f(100.0f, 100.0f, 0.0f) }
 	};
+
 }
 
-void Cube::drawTriangle(glMath::vec3f p1,glMath::vec3f p2, glMath::vec3f p3, int color=1)
+void Cube::drawTriangle(glMath::vec3f p1, glMath::vec3f p2, glMath::vec3f p3, int color=1)
 {
 	BLA(p1.x,p1.y,p2.x,p2.y, color);
 	BLA(p2.x,p2.y,p3.x,p3.y, color);
@@ -79,19 +85,29 @@ void Cube::putpixel(float x, float y, int color)
 }
 
 void Cube::render() {
-	glMath::mat4f model(1.0f);
-	// model = glMath::translate(model, glMath::vec3f(100, 100, 0));
-	glMath::mat4f view(1.0f);
-	// view = glMath::perspective(glMath::radians(45.f), 640.f/640, 0.1f, 100.f);
-	// model = view * model;
-	
+	for (auto &i : meshCube.tris) {
+		drawTriangle(i.p[0], i.p[1], i.p[2]);
+	}
+}
+
+void Cube::translate(glMath::vec3f p)
+{
+	model = glMath::translate<float>(model, p);
+}
+
+void Cube::rotate(glMath::vec3f p, float angle)
+{
+	model = glMath::rotate<float>(model,(float)(PIE/180)*(angle), p);
+}
+
+void Cube::updateVertices()
+{
 	for (auto &i : meshCube.tris) {
 		for (auto &j : i.p)
 		{
 			glMath::vec3f v(j.x, j.y, j.z);
 			v =  model * v;
-			j = glMath::vec3f(v.x, v.y, v.z);
+			j = glMath::vec3f(v.x , v.y, v.z);
 		}
-		drawTriangle(i.p[0], i.p[1], i.p[2]);
 	}
 }
